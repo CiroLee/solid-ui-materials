84d034299193b46c5eca88caca41b3ada980c5a1
CiroLee <ciromail@126.com>
2023-07-15 14:08:50 +0800

    style: opt home style

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,35 +5,48 @@ import { A } from '@solidjs/router';
 const Home: Component = () => {
   return (
     <div class={style.home} h-full flex flex-col flex-center>
-      <div class={style['slogan']} flex flex-center flex-wrap overflow-hidden>
-        <span font-bold text-32px>
-          No
-        </span>
-        <span class={style['inline-code']}>install</span>
-        <span font-bold text-32px>
-          Hight
-        </span>
-        <span class={style['inline-code']}>re-useable</span>
-        <span font-bold text-32px>
-          Easy
-        </span>
-        <span mx-1>to modify</span>
-        <span font-bold text-32px>
-          Just
-        </span>
-        <span class={style['inline-code']}>copy</span> and <span class={style['inline-code']}>paste</span> codes to your
-        project.
+      <div class={style['slogan']}>
+        <p>
+          <span font-bold text-32px>
+            No
+          </span>
+          <span class={style['inline-code']}>install</span>
+        </p>
+        <p>
+          <span font-bold text-32px>
+            Zero
+          </span>
+          <span class={style['inline-code']}>3rd dependencies</span>
+        </p>
+        <p>
+          <span font-bold text-32px>
+            Hight
+          </span>
+          <span class={style['inline-code']}>re-useable</span>
+        </p>
+        <div>
+          <span font-bold text-32px>
+            Easy
+          </span>
+          <span ml-1>to</span>
+          <span class={style['inline-code']}>modify</span>
+        </div>
+        <p>
+          <span font-bold text-32px>
+            Just
+          </span>
+          <span class={style['inline-code']}>copy</span> and <span class={style['inline-code']}>paste</span> codes to
+          your project.
+        </p>
       </div>
-      <div mt-12>
-        <span mr-2>visit on</span>
-        <A
-          class="decoration-none text-inherit transition-all hover:text-blue-600"
-          href="https://github.com/CiroLee/solid-ui-materials"
-          target="_blank">
-          <SIcon name="github-fill" size="22px" class="mr-1" />
-          Github
-        </A>
-      </div>
+      <A
+        class="text-16px flex flex-center absolute top-4 right-4 decoration-none text-inherit transition-all 
+        hover:text-blue-600"
+        href="https://github.com/CiroLee/solid-ui-materials"
+        target="_blank">
+        <SIcon name="github-fill" size="22px" class="mr-1" />
+        Github
+      </A>
     </div>
   );
 };
