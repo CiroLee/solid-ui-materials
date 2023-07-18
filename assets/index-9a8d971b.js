d97f1448f45a1845833620e5a2d96abd6cdadaba
CiroLee <CiroMail@126.com>
2023-07-18 18:25:00 +0000

    Deploying to gh-pages from @ CiroLee/solid-ui-materials@371f89b3796febb3a66234c8c6fcfb23e9c1d756 🚀

--- assets/index-9a8d971b.js
+++ assets/index-9a8d971b.js
@@ -1,9 +1,8 @@
-import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";import{S as b,C as c,a as x}from"./index-b9c48bb1.js";import{S as l}from"./index-333db6f8.js";const S=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
+import{d as y,i as n,c as a,S as e,t as p,e as b}from"./index-73775d6a.js";import{S as x,C as c,a as w}from"./index-acd75915.js";import{S as l}from"./index-4a159689.js";const S=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">SButton</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path&#x27;</span>;
 <span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
   <span class="hljs-keyword">return</span> (
     <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
-      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;default&quot;</span>&gt;</span>default<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;primary&quot;</span>&gt;</span>primary<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;success&quot;</span>&gt;</span>success<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;warn&quot;</span>&gt;</span>warn<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
@@ -21,6 +20,30 @@ import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";impor
 <span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
   <span class="hljs-keyword">return</span> (
     <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">outline</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;primary&quot;</span>&gt;</span>
+        primary
+      <span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">outline</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;success&quot;</span>&gt;</span>
+        success
+      <span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">outline</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;warn&quot;</span>&gt;</span>
+        warn
+      <span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">outline</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;danger&quot;</span>&gt;</span>
+        danger
+      <span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+  );
+};
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-title class_">App</span>;
+</code></pre>
+`,q=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
+<span class="hljs-keyword">import</span> <span class="hljs-title class_">SButton</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path&#x27;</span>;
+<span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
+  <span class="hljs-keyword">return</span> (
+    <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;tiny&quot;</span>&gt;</span>tiny<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;small&quot;</span>&gt;</span>small<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;medium&quot;</span>&gt;</span>medium<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">size</span>=<span class="hljs-string">&quot;large&quot;</span>&gt;</span>large<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
@@ -30,7 +53,7 @@ import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";impor
 
 <span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-title class_">App</span>;
 </code></pre>
-`,w=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
+`,B=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">SButton</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path&#x27;</span>;
 <span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
   <span class="hljs-keyword">return</span> (
@@ -44,12 +67,14 @@ import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";impor
 
 <span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-title class_">App</span>;
 </code></pre>
-`,q=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
+`,_=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">SButton</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path&#x27;</span>;
 <span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
   <span class="hljs-keyword">return</span> (
     <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
-      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">block</span>&gt;</span>block<span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">outline</span> <span class="hljs-attr">block</span>&gt;</span>
+        block
+      <span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
       <span class="hljs-tag">&lt;<span class="hljs-name">SButton</span> <span class="hljs-attr">block</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;primary&quot;</span>&gt;</span>
         block
       <span class="hljs-tag">&lt;/<span class="hljs-name">SButton</span>&gt;</span>
@@ -62,7 +87,7 @@ import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";impor
 
 <span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-title class_">App</span>;
 </code></pre>
-`,B=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
+`,f=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">SButton</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path&#x27;</span>;
 <span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
   <span class="hljs-keyword">return</span> (
@@ -109,7 +134,7 @@ import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";impor
   }
 }
 </code></pre>
-`,f=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
+`,$=`<pre class="hljs"><code><span class="hljs-keyword">import</span> { <span class="hljs-keyword">type</span> <span class="hljs-title class_">Component</span> } <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;solid-js&#x27;</span>;
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">SButton</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path&#x27;</span>;
 <span class="hljs-keyword">const</span> <span class="hljs-title class_">App</span>: <span class="hljs-title class_">Component</span> = <span class="hljs-function">() =&gt;</span> {
   <span class="hljs-keyword">return</span> (
@@ -149,4 +174,4 @@ import{d as m,i as n,c as a,S as e,t as p,e as y}from"./index-82502e81.js";impor
   }
 }
 </code></pre>
-`;const _=p("<div flex items-center justify-between><h3>SButton</h3><div><span hover:text-blue-600 text-gray cursor-pointer mr-4>code"),$=p("<h4>button type"),v=p('<div class="children:mr-2 children:mb-2">'),C=p("<h4>size"),r=p('<div class="flex items-center children:mr-2 children:mb-2">'),A=p("<h4>shape"),z=p("<h4>block"),P=p("<div w-400px p-12px border border-solid border-gray-300 rounded children:mb-2>"),I=p("<h4>usage - button group"),j=p('<div class="button-group">'),D=p("<h4>usage - button with icon"),i=p("<span>Add"),E=p("<span>loading");function G(){const u=[{name:"tsx",path:"components/SButton/index.tsx"},{name:"scss",path:"components/SButton/index.scss"}],[g,h]=m(!1);return[(()=>{const s=_(),t=s.firstChild,o=t.nextSibling,d=o.firstChild;return d.$$click=()=>h(!0),n(o,a(b,{path:"SButton",name:"source"}),null),s})(),$(),(()=>{const s=v();return n(s,a(l,{type:"default",children:"default"}),null),n(s,a(l,{type:"primary",children:"primary"}),null),n(s,a(l,{type:"success",children:"success"}),null),n(s,a(l,{type:"warn",children:"warn"}),null),n(s,a(l,{type:"danger",children:"danger"}),null),n(s,a(l,{type:"text",children:"text"}),null),n(s,a(l,{disabled:!0,children:"primary"}),null),s})(),a(c,{content:S}),C(),(()=>{const s=r();return n(s,a(l,{size:"small",children:"small"}),null),n(s,a(l,{size:"medium",children:"medium"}),null),n(s,a(l,{size:"large",children:"large"}),null),s})(),a(c,{content:k}),A(),(()=>{const s=r();return n(s,a(l,{shape:"default",children:"default"}),null),n(s,a(l,{shape:"round",children:"round"}),null),n(s,a(l,{shape:"circle",children:"C"}),null),s})(),a(c,{content:w}),z(),(()=>{const s=P();return n(s,a(l,{block:!0,children:"block"}),null),n(s,a(l,{block:!0,type:"primary",children:"block"}),null),n(s,a(l,{block:!0,shape:"round",type:"primary",children:"block"}),null),s})(),a(c,{content:q}),I(),(()=>{const s=j();return n(s,a(l,{type:"primary",children:"apple"}),null),n(s,a(l,{type:"primary",children:"pear"}),null),n(s,a(l,{type:"primary",children:"banana"}),null),s})(),(()=>{const s=j();return n(s,a(l,{type:"primary",shape:"round",children:"apple"}),null),n(s,a(l,{type:"primary",shape:"round",children:"pear"}),null),n(s,a(l,{type:"primary",shape:"round",children:"banana"}),null),s})(),a(c,{content:B}),D(),(()=>{const s=r();return n(s,a(l,{type:"primary",get children(){return[a(e,{name:"add-circle-line",size:"16px"}),(()=>{const t=i();return t.style.setProperty("padding-left","4px"),t})()]}}),null),n(s,a(l,{type:"primary",get children(){return[(()=>{const t=i();return t.style.setProperty("padding-right","4px"),t})(),a(e,{name:"add-circle-line",size:"16px"})]}}),null),n(s,a(l,{type:"primary",loading:!0,class:"button-loading",get children(){return[a(e,{name:"loader-line",size:"16px"}),(()=>{const t=E();return t.style.setProperty("padding-left","4px"),t})()]}}),null),s})(),a(c,{content:f}),a(x,{get show(){return g()},tabs:u,onCancel:()=>h(!1)})]}y(["click"]);export{G as default};
+`;const v=p("<div flex items-center justify-between><h3>SButton</h3><div><span hover:text-blue-600 text-gray cursor-pointer mr-4>code"),C=p("<h4>button type"),j=p('<div class="children:mr-2 children:mb-2">'),A=p("<h4>outline"),z=p("<h4>size"),r=p('<div class="flex items-center children:mr-2 children:mb-2">'),P=p("<h4>shape"),I=p("<h4>block"),D=p("<div w-400px p-12px border border-solid border-gray-300 rounded children:mb-2>"),E=p("<h4>usage - button group"),u=p('<div class="button-group">'),L=p("<h4>usage - button with icon"),i=p("<span>Add"),V=p("<span>loading");function J(){const g=[{name:"tsx",path:"components/SButton/index.tsx"},{name:"scss",path:"components/SButton/index.scss"}],[d,h]=y(!1);return[(()=>{const s=v(),t=s.firstChild,o=t.nextSibling,m=o.firstChild;return m.$$click=()=>h(!0),n(o,a(x,{path:"SButton",name:"source"}),null),s})(),C(),(()=>{const s=j();return n(s,a(l,{type:"primary",children:"primary"}),null),n(s,a(l,{type:"success",children:"success"}),null),n(s,a(l,{type:"warn",children:"warn"}),null),n(s,a(l,{type:"danger",children:"danger"}),null),n(s,a(l,{type:"text",children:"text"}),null),n(s,a(l,{disabled:!0,children:"primary"}),null),s})(),a(c,{content:S}),A(),(()=>{const s=j();return n(s,a(l,{outline:!0,type:"primary",children:"primary"}),null),n(s,a(l,{outline:!0,type:"success",children:"success"}),null),n(s,a(l,{outline:!0,type:"warn",children:"warn"}),null),n(s,a(l,{outline:!0,type:"danger",children:"danger"}),null),s})(),a(c,{content:k}),z(),(()=>{const s=r();return n(s,a(l,{size:"tiny",children:"tiny"}),null),n(s,a(l,{size:"small",children:"small"}),null),n(s,a(l,{size:"medium",children:"medium"}),null),n(s,a(l,{size:"large",children:"large"}),null),s})(),a(c,{content:q}),P(),(()=>{const s=r();return n(s,a(l,{shape:"default",children:"default"}),null),n(s,a(l,{shape:"round",children:"round"}),null),n(s,a(l,{shape:"circle",children:"C"}),null),s})(),a(c,{content:B}),I(),(()=>{const s=D();return n(s,a(l,{outline:!0,block:!0,children:"block"}),null),n(s,a(l,{block:!0,type:"primary",children:"block"}),null),n(s,a(l,{block:!0,shape:"round",type:"primary",children:"block"}),null),s})(),a(c,{content:_}),E(),(()=>{const s=u();return n(s,a(l,{type:"primary",children:"apple"}),null),n(s,a(l,{type:"primary",children:"pear"}),null),n(s,a(l,{type:"primary",children:"banana"}),null),s})(),(()=>{const s=u();return n(s,a(l,{type:"primary",shape:"round",children:"apple"}),null),n(s,a(l,{type:"primary",shape:"round",children:"pear"}),null),n(s,a(l,{type:"primary",shape:"round",children:"banana"}),null),s})(),a(c,{content:f}),L(),(()=>{const s=r();return n(s,a(l,{type:"primary",get children(){return[a(e,{name:"add-circle-line",size:"16px"}),(()=>{const t=i();return t.style.setProperty("padding-left","4px"),t})()]}}),null),n(s,a(l,{type:"primary",get children(){return[(()=>{const t=i();return t.style.setProperty("padding-right","4px"),t})(),a(e,{name:"add-circle-line",size:"16px"})]}}),null),n(s,a(l,{type:"primary",loading:!0,class:"button-loading",get children(){return[a(e,{name:"loader-line",size:"16px"}),(()=>{const t=V();return t.style.setProperty("padding-left","4px"),t})()]}}),null),s})(),a(c,{content:$}),a(w,{get show(){return d()},tabs:g,onCancel:()=>h(!1)})]}b(["click"]);export{J as default};
